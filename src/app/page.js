e7627c4aad066fce26663f3d3c13cd519d71d420
Bayna0999 <baynaa123455@gmail.com>
2025-02-25 23:22:58 +0800

    component nemsen input

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import { Circle } from "@/components";
+import { Circle, Input } from "@/components";
 export default function Home() {
   return (
     <div className="flex w-full h-screen justify-center items-center">
-      <div className="bg-[#F3F4F6] w-full h-full relative"></div>
-      <div className="bg-black w-full h-full relative"></div>
+      <div className="bg-[#F3F4F6] w-full h-full relative">
+        <Input text={"hello"} src={"icon.png"}/>
+      </div>
+      <div className="bg-black w-full h-full relative">
+      <Input/>
+      </div>
       <Circle classname="w-[140px] h-[140px]" />
     </div>
   );
